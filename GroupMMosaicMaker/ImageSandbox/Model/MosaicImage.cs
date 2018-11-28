f8cc685adfa1d4797d8f27541da35fbb08a69110
Victoria Jenkins <vjenkin2@my.westga.edu>
2018-11-28 15:26:48 -0500

    added class

--- GroupMMosaicMaker/ImageSandbox/Model/MosaicImage.cs
+++ GroupMMosaicMaker/ImageSandbox/Model/MosaicImage.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 
@@ -58,5 +59,120 @@ namespace ImageSandbox.Model
             newImage.SetSource(inputstream);
             return newImage;
         }
+        public void CreateSolidMosaic(byte[] sourcePixels, uint imageWidth, uint imageHeight, int blockSize, bool isGrid)
+        {
+            var y = 0;
+            while (y < imageHeight)
+            {
+                var x = 0;
+                while (x < imageWidth)
+                {
+                    var XStoppingPoint = this.UpdateStoppingPoint(imageWidth, x, blockSize);
+
+                    var YStoppingPoint = this.UpdateStoppingPoint(imageHeight, y, blockSize);
+
+                    this.setNewColorValue(sourcePixels, imageWidth, imageHeight, y, YStoppingPoint, x, XStoppingPoint, isGrid);
+
+                    x += blockSize;
+                }
+                y += blockSize;
+            }
+        }
+
+        private void setNewColorValue(byte[] sourcePixels, uint imageWidth, uint imageHeight, int startingYPoint, int YStoppingPoint, int startingXPoint, int XStoppingPoint, bool isGrid)
+        {
+            var averageColor =
+                this.FindAverageColor(sourcePixels, imageWidth, imageHeight, startingYPoint,
+                    YStoppingPoint, startingXPoint, XStoppingPoint);
+            for (var currentYPoint = startingYPoint; currentYPoint < YStoppingPoint; currentYPoint++)
+            {
+                for (var currentXPoint = startingXPoint; currentXPoint < XStoppingPoint; currentXPoint++)
+                {
+                    var pixelColor = getPixelBgra8(sourcePixels, currentYPoint, currentXPoint, imageWidth, imageHeight);
+
+                    if (isGrid)
+                    {
+                        if (currentYPoint == startingYPoint || YStoppingPoint == currentYPoint
+                                                || currentXPoint == startingXPoint || XStoppingPoint == currentXPoint)
+                        {
+                            pixelColor = Colors.White;
+
+                        }
+                        else
+                        {
+                            pixelColor.R = averageColor.R;
+                            pixelColor.B = averageColor.B;
+                            pixelColor.G = averageColor.G;
+                        }
+                    }
+                    else
+                    {
+                        pixelColor.R = averageColor.R;
+                        pixelColor.B = averageColor.B;
+                        pixelColor.G = averageColor.G;
+                    }
+
+
+                    this.setPixelBgra8(sourcePixels, currentYPoint, currentXPoint, pixelColor, imageWidth, imageHeight);
+                }
+            }
+        }
+        private void setPixelBgra8(byte[] pixels, int x, int y, Color color, uint width, uint height)
+        {
+            var offset = (x * (int)width + y) * 4;
+            pixels[offset + 2] = color.R;
+            pixels[offset + 1] = color.G;
+            pixels[offset + 0] = color.B;
+        }
+        private int UpdateStoppingPoint(uint maxValue, int coordinate, int blockSize)
+        {
+            var CoordinateStoppingPoint = coordinate + blockSize;
+            if (CoordinateStoppingPoint > maxValue)
+            {
+                CoordinateStoppingPoint = (int)maxValue;
+            }
+
+            return CoordinateStoppingPoint;
+        }        
+        public Color FindAverageColor(byte[] sourcePixels, uint imageWidth, uint imageHeight, int startingYPoint, int YStoppingPoint, int startingXPoint,
+            int XStoppingPoint)
+        {
+            var pixelCount = 0.0;
+            var totalRed = 0.0;
+            var totalBlue = 0.0;
+            var totalGreen = 0.0;
+
+            for (var currentYPoint = startingYPoint; currentYPoint < YStoppingPoint; currentYPoint++)
+            {
+                for (var currentXPoint = startingXPoint; currentXPoint < XStoppingPoint; currentXPoint++)
+                {
+                    pixelCount++;
+                    var pixelColor = getPixelBgra8(sourcePixels, currentYPoint, currentXPoint, imageWidth, imageHeight);
+                    totalRed += pixelColor.R;
+                    totalBlue += pixelColor.B;
+                    totalGreen += pixelColor.G;
+                }
+            }
+
+            var averageRed = totalRed / pixelCount;
+            var averageBlue = totalBlue / pixelCount;
+            var averageGreen = totalGreen / pixelCount;
+
+            var newColor = new Color();
+            newColor.R = (byte)averageRed;
+            newColor.B = (byte)averageBlue;
+            newColor.G = (byte)averageGreen;
+            return newColor;
+        }
+        public  static Color getPixelBgra8(byte[] pixels, int x, int y, uint width, uint height)
+        {
+            var offset = (x * (int)width + y) * 4;
+            var r = pixels[offset + 2];
+            var g = pixels[offset + 1];
+            var b = pixels[offset + 0];
+            return Color.FromArgb(0, r, g, b);
+        }
     }
 }
+
+
