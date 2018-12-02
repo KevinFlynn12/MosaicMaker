1579e1411d13d9254d5804a99cee4612179d2a94
KevinFlynn12 <kflynn2@my.westga.edu>
2018-12-01 20:21:21 -0500

    Refactoring and revisions

---
+++ GroupMMosaicMaker/ImageSandbox/Model/folderImage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+using ImageSandbox.Util;
+
+namespace ImageSandbox.Model
+{
+    public class FolderImage
+    {
+        public WriteableBitmap imageBitmap { get; private set; }
+
+
+        public FolderImage(WriteableBitmap loadedBitmap)
+        {
+            this.imageBitmap = loadedBitmap;
+        }
+
+        public Color FindAverageColor()
+        {
+            var imageWidth = (uint) this.imageBitmap.PixelWidth;
+
+            var imageHeight = (uint) this.imageBitmap.PixelHeight;
+
+            var sourcePixels = this.imageBitmap.PixelBuffer.ToArray();
+
+            var averageColor = ImageAverageColor.FindAverageColorOfEntireImage(sourcePixels, imageWidth, imageHeight);
+
+            return averageColor;
+        }
+
+    }
+}
---
+++ GroupMMosaicMaker/ImageSandbox/Util/ColorDIfference.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+
+namespace ImageSandbox.Util
+{
+    class ColorDIfference
+    {
+        private static int GetColorDifference(Color color, Color baseColor)
+        {
+            int a = color.A - baseColor.A,
+                r = color.R - baseColor.R,
+                g = color.G - baseColor.G,
+                b = color.B - baseColor.B;
+            return a * a + r * r + g * g + b * b;
+        }
+
+    }
+}
